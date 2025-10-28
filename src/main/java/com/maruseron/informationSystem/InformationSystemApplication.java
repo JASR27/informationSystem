49c00dae29f5f7d54bae739d40c11a83e459a3ad
JASR27 <sayeghramosjorge@gmail.com>
2025-10-28 15:48:09 -0400

    Cambio del cors
    Modifique el cors para permitir explicitamente solicitudes put que no me estaba dejando

--- src/main/java/com/maruseron/informationSystem/InformationSystemApplication.java
+++ src/main/java/com/maruseron/informationSystem/InformationSystemApplication.java
@@ -14,15 +14,18 @@ public class InformationSystemApplication {
 		SpringApplication.run(InformationSystemApplication.class, args);
 	}
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080",
-                                        "http://localhost:5173");
-            }
-        };
-    }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
+
 }
